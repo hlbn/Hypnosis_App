63f4b1abdcfd345403aa0c2542be39a589430d5b
Adam Hlubina <adam.hlubina@eman.cz>
2023-05-20 17:38:38 +0200

    Initial Commit

---
+++ Hypnosis/HypnosisApp.swift
@@ -0,0 +1,17 @@
+//
+//  HypnosisApp.swift
+//  Hypnosis
+//
+//  Created by Adam Hlubina on 20.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct HypnosisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
