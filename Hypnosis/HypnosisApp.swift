b0d2fce2bd52dc9dfe322947578208038c3561a4
Adam Hlubina <adam.hlubina@eman.cz>
2023-05-20 19:45:54 +0200

    feat: add firebase

--- Hypnosis/HypnosisApp.swift
+++ Hypnosis/HypnosisApp.swift
@@ -6,12 +6,35 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+
+// MARK: - SwiftUI App
 
 @main
 struct HypnosisApp: App {
+    
+    // MARK: - Properties
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    
+    // MARK: - Body
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+}
+
+
+// MARK: - AppDelegate
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
 }
